87fe098b4c5b5c4485deaff88cfc1dada062caa7
bf-san <bf-san@proton.me>
2024-11-21 15:32:56 +0100

    fix: timeframe set to weeks

--- src/components/statistics/Staked.tsx
+++ src/components/statistics/Staked.tsx
@@ -12,7 +12,7 @@ import { useTranslation } from 'react-i18next';
 
 const Staked = () => {
 	const { t } = useTranslation('staking');
-	const [timeframe, setTimeframe] = useState<Timeframe>('day');
+	const [timeframe, setTimeframe] = useState<Timeframe>('week');
 
 	const { data: statistics = [] } = useStakingStatistics(timeframe);
 	const { data: currentStatistic } = useStakedStatisticsCurrent();
